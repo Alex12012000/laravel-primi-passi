ba5a1813e451c7bfcc5c40a81fa670caf446bf6b
Alex12012000 <arcex.29@gmail.com>
2022-08-23 14:14:23 +0200

    finito + bonus

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="{{ route('home') }}">
+        Torna alla pagina iniziale!
+    </a>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, at culpa architecto expedita officiis error dolorem dolores sunt saepe tenetur maiores eum animi aut quia perspiciatis cupiditate rem, eius ut.</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+
+    $user = [
+        "name" => 'Alex',
+        "lastname" => 'Airinei'
+    ];
+        
+
+    return view('home', $user);
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
